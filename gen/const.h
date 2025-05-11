1b5e0edddce509d407848c9c0209e33f3aeabb80
lemonilemon <imlemonilemon@gmail.com>
2025-05-11 09:45:46 +0800

    update small testcases for test

--- gen/const.h
+++ gen/const.h
@@ -6,9 +6,10 @@ using namespace std;
 
 const vector<vector<int>> PRESET = {
     {14, 14, 0, 0, 9, 0},  // 0 (subtask 1)
-    {14, 14, 5, 14, 9, 9}, // 1 (subtask 2)
-    {20, 0, 5, 0, 2, 9},   // 2 (subtask 3)
-    {20, 20, 5, 0, 2, 9},  // 3 (subtask 4)
+    {14, 14, 5, 14, 2, 9}, // 1 (subtask 2)
+    {14, 0, 5, 0, 2, 9},   // 2 (subtask 3)
+    {14, 14, 5, 0, 2, 9},  // 3 (subtask 4)
+    {14, 14, 5, 14, 2, 9}, // 4 (subtask 5)
 };
 
 const int MINKEY = 1;
